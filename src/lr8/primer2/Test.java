5a95f9f7e14de146638608af4dd22f3412bed0a1
Yuliya <uuu4891@gmail.com>
2024-04-11 14:56:40 +0500

    lr8

--- src/lr8/primer2/Test.java
+++ src/lr8/primer2/Test.java
@@ -3,9 +3,22 @@
 соответствующие условию - в исходном файле содержится две строки в
 формате UTF-8 и 5 чисел типа double. В результирующий файл переписать
 вторую строку и положительные числа.*/
- lr8.primer2;
+package lr8.primer2;
+import java.io.File;
+import java.io.IOException;
 
 public class Test {
+    public static void main(String[] args) throws IOException {
+        try {
+
+            // Создание файла на диске
+            File f2 = new File("C:/Users/DearUser3/IdeaProjects/FirstJavaProject/primer2.txt");
+            f2.createNewFile();
+
+        } catch (IOException e) {
+
+        }
 
 
-}
+        }
+    }
