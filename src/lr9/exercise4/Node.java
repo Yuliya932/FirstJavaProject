3d937363b6c9319ad8fccd09851d7f192b0f9833
Yuliya <uuu4891@gmail.com>
2024-04-24 00:48:52 +0500

    lr9

--- src/lr9/exercise4/Node.java
+++ src/lr9/exercise4/Node.java
@@ -12,7 +12,7 @@ public class Node {
     //Класс - структура элемента списка
     public int value; // значение
 
-    public Node next; // поле - ссылка указатель на следущий элемент
+    public Node next; // поле - ссылка указатель на следующий элемент
 
     Node(int value, Node next) { // конструктор класса
         this.value = value;
