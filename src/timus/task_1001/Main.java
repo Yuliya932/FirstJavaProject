69ec88a53a6c8df10cfce2f857a5ab8a384a8a67
Yuliya <uuu4891@gmail.com>
2024-05-04 02:18:57 +0500

    timus

--- src/timus/task_1001/Main.java
+++ src/timus/task_1001/Main.java
@@ -3,19 +3,19 @@
 Ограничение памяти: 64 МБ
 Эта задача настолько проста, что авторы даже поленились сочинить для нее условие!
 Исходные данные
-Входной поток содержит набор целых чисел Ai (0 ≤ Ai ≤ 1018), отделённых друг от друга произвольным количеством пробелов и переводов строк. Размер входного потока не превышает 256 КБ.
+Входной поток содержит набор целых чисел Ai (0 ≤ Ai ≤ 1018),
+отделённых друг от друга произвольным количеством пробелов и переводов строк.
+Размер входного потока не превышает 256 КБ.
 Результат
-Для каждого числа Ai, начиная с последнего и заканчивая первым, в отдельной строке вывести его квадратный корень не менее чем с четырьмя знаками после десятичной точки.*/
+Для каждого числа Ai, начиная с последнего и заканчивая первым,
+в отдельной строке вывести его квадратный корень не менее чем с четырьмя знаками после десятичной точки.
+2297.0716
+936297014.1164
+0.0000
+37.7757*/
 package timus.task_1001;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,63 +27,141 @@ public class Main {
                     oj ? new BufferedReader(new InputStreamReader(System.in)) :
                             new BufferedReader(new FileReader(inputFileName)); //данные считаются из файла
 
-                String[] ints = new String[20];
-                String str;
-                 int h = 0;
-//                List<Integer> list = new ArrayList<>();
-//                list.add(5);
-                int words = 0;
+            String readLine;//можно убрать ="", и в ней будет null
+            String[] strings2;
 
-                String readLine = "";
-                for (int i = 0; ((readLine = bufferedReader.readLine()) != null||(readLine = bufferedReader.readLine()) != ""); i++){
-                    if(bufferedReader.readLine()  != null){
-                        String[] strings = readLine.split("\\P{Alnum}+");
+            while ((readLine = bufferedReader.readLine()) != null) { //перебираем в цикле каждую строку
+                String[] strings = readLine.split("\\r?\\n");
+//                String[] strings00;
+                for (String j : strings) {
+//                    strings00[]=strings[];
 
+                    if (j.split("\\s+") != null) {
+                        strings2 = j.split("\\P{Alnum}+");
+                        for (String k : strings2) {
+                                try {
+//                                     System.out.println(Double.parseDouble(k.toString()));
+                                    double d = Double.parseDouble(k.toString());
+//                                    double a =  Math.round( Math.sqrt(d)*10000)/10000.0000f;
+//                                    double a =   (Math.sqrt(d)*10000))/10000;
+//                                     String b = "%f!"+ (a)f;
+                            System.out.println(String.format("%.4f",Math.sqrt(d)));
 
-                        if (strings[i] != null) {
+                                } catch (Exception e) {
+                                }
 
-                            for (int j = 0; j < strings.length; j++) {
+//                            int m = 0;
+//                            double[] array = new double[strings2.length];
+//                            array[m]=Double.parseDouble(k.toString());
+//                            m++;
 
-//                    for (String j : strings ){
 //
-//                        if(readLine.split("\\s+")!=null) {
+//                            double[] array;
+//                            array[m] = Double.parseDouble(k.toString());
+//                            m++;
+//                            double d = Double.parseDouble(k.toString());
+//                            long a = (long)Math.round(Math.sqrt(d*10000));
+//                            System.out.println(a/10000);
 
-//                            String[] strings2[j];
-//                            = strings[i];
-//
-//                            if ((readLine.split("\\P{Alnum}+"))<>"")
-//
-//                            h = Integer.parseInt(strings2[j]); // считываем, преобразуем
-//                            System.out.println(h);
-
-                                System.out.println(strings[j]);
-                            }
-
-
-//                            System.out.println(j);
-                            //System.out.println(h);
-
-                            words++;
+//                                    System.out.println(a.format("%.4f%n",(double)a/10000));
+//                            System.out.println(array);
                         }
+
                     }
 
-//                        while ((readLine = bufferedReader.readLine()) != null) { //перебираем в цикле каждую строку
-//                            if(numberOfFriends == 0){
-//                                numberOfFriends = Integer.parseInt(readLine); // считываем, преобразуем
-//                                continue;
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+//            Scanner in = new Scanner(System.in);
+//            PrintWriter out = new PrintWriter(System.out);
 //
+//            double[] al = new double[20];
+//            int i = 0;
+//            while(in.hasNextDouble()){
+//                al[i++] = in.nextDouble();
+//            }
+//
+//            for(;i>0;i++){
+//                long i1 = (long)Math.round(Math.sqrt(al[i-1])*10000);
+//                out.format("%.4f%n",(double)i1/10000);
+//            }
+//            out.flush();
+//            in.close();
+
+
+
+
+//                String[] ints = new String[20];
+//                String str;
+//                 int h = 0;
+////                List<Integer> list = new ArrayList<>();
+////                list.add(5);
+//                int words = 0;
+//
+//                String readLine = "";
+//
+//            while (bufferedReader.readLine() != null){
+//
+//
+//                for (int i = 0; ((readLine = bufferedReader.readLine()) != null); i++) {
+//                    if (bufferedReader.readLine() != null) {
+//                        String[] strings = readLine.split("\\s+");
+//
+//
+//                        if (strings[i] != null) {
+//
+//                            for (int j = 0; j < strings.length; j++) {
+//
+////                    for (String j : strings ){
+////
+////                        if(readLine.split("\\s+")!=null) {
+//
+////                            String[] strings2[j];
+////                            = strings[i];
+////
+////                            if ((readLine.split("\\P{Alnum}+"))<>"")
+////
+////                            h = Integer.parseInt(strings2[j]); // считываем, преобразуем
+////                            System.out.println(h);
+//
+//                                System.out.println(strings[j]);
 //                            }
-//                            if (readLine.contains("+")){ //true - если содержит +, false - если нет
-//                                numberOfPairs++;
-//                            }
+//
+//
+////                            System.out.println(j);
+//                            //System.out.println(h);
+//
+//                            words++;
 //                        }
-//                        String[] array = strings.nextLine().split(" ");
-//                        words = words + array.length;
-//                        System.out.println(Arrays.toString(strings));
-//                        for (int k = 0; k < 20 ; k++){
-//                            ints[k] = Integer.parseInt(strings[k]);
-//                            ints[k] = Long.parseLong(strings[k]);
-                        }
+//                    }
+//
+////                        while ((readLine = bufferedReader.readLine()) != null) { //перебираем в цикле каждую строку
+////                            if(numberOfFriends == 0){
+////                                numberOfFriends = Integer.parseInt(readLine); // считываем, преобразуем
+////                                continue;
+////
+////                            }
+////                            if (readLine.contains("+")){ //true - если содержит +, false - если нет
+////                                numberOfPairs++;
+////                            }
+////                        }
+////                        String[] array = strings.nextLine().split(" ");
+////                        words = words + array.length;
+////                        System.out.println(Arrays.toString(strings));
+////                        for (int k = 0; k < 20 ; k++){
+////                            ints[k] = Integer.parseInt(strings[k]);
+////                            ints[k] = Long.parseLong(strings[k]);
+//                }
+//            }
 
 //                    }
 
