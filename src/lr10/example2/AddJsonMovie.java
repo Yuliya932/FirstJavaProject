dbf861832ca8d4b9b6cb7c32e9c2d2b9d4fd8a70
Yuliya <uuu4891@gmail.com>
2024-05-03 16:44:05 +0500

    lr10

--- src/lr10/example2/AddJsonMovie.java
+++ src/lr10/example2/AddJsonMovie.java
@@ -1,31 +1,53 @@
 /*3)	Добавьте функционал для добавления нового фильма в массив*/
 package lr10.example2;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class AddJsonMovie {
+    public static void main(String[] args) {
 
-//    try {
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Хотите добавить фильмы в список? (y/n)");
-//        String c = in.nextLine();
-//
-//        if ("y".equals(c)) {
-//
-//
-//
-//            System.out.println("Введите название фильма: ");
-//            System.out.println("Введите режиссера фильма: ");
-//            System.out.println("Введите год выхода фильма: ");
-//
-//
-//
-//        }
-//
-//
-//
-//}
-}
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("src/lr10/example2/example-json-movies.json"));
+            JSONObject library = (JSONObject) obj;
+            JSONArray movies = (JSONArray) library.get("movies");
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Хотите добавить фильмы в список? (y/n)");
+            String c = in.nextLine();
+
+            if ("y".equals(c)) {
+                JSONObject newMovie = new JSONObject();
+
+                System.out.println("Введите название фильма: ");
+                newMovie.put("title", in.nextLine());
+                System.out.println("Введите режиссера фильма: ");
+                newMovie.put("director", in.nextLine());
+                System.out.println("Введите год выхода фильма: ");
+                newMovie.put("year", in.nextLine());
+
+                movies.add(newMovie);
+
+                library.put("movies", movies);
+
+                try(FileWriter file = new FileWriter("src/lr10/example2/example-json-movies.json")){
+                file.write(library.toJSONString());
+                System.out.println("Фильм успешно добавлен в Json файл");
+            }}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 
 //Подсказки
 //        2.	Для добавления новой книги в массив необходимо использовать метод JSONArray.add() для добавления нового объекта JSONObject в массив JSONArray.
