d3e7357896fcd1a26aa2a254453d7c9c461702a5
Yuliya932 <uuu4891@gmail.com>
2024-05-15 23:04:04 +0500

    reserve

---
+++ src/reserve/ClientInfo.java
@@ -0,0 +1,29 @@
+package reserve;
+
+import java.time.LocalDate;
+
+public class ClientInfo {
+    private final String name;
+    private final LocalDate birthDate;
+
+    public ClientInfo(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
---
+++ src/reserve/ParserMT.java
@@ -0,0 +1,54 @@
+package reserve;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class ParserMT {
+    private static Document getPage() throws IOException {
+        String url = "https://www.metallotorg.ru/info/pricelists/ekaterinburg/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        return page;
+    }
+
+    public static void main(String[] args) throws IOException {
+//        System.out.println(getPage());
+        Document page = getPage();
+        //css query language
+//        Element tableWth = page.select("div[id=content]").first();
+//            System.out.println(tableWth);
+        Element tablePrs = page.select("Table[id=MyTable]").first();
+//            System.out.println(tableWth);
+//        System.out.println(tablePrs);
+        Elements names = tablePrs.select("thead[id=TheHead]");
+        Elements values = tablePrs.select("tbody[id=TheBody]");
+
+//        System.out.println(names);
+//        System.out.println(values);
+
+        int index = 0;
+
+        for (Element name : names) {
+//            String dateSting = name.select("th[id=dt]").text();
+//            String date = getDateFromString(dateSting);
+//            System.out.println("Дата\tЯвления\tТемпература \tДавл\tВлажность\tВетер");
+//            System.out.print(date +"\t");
+//            int iterationCount = printPartValues(values, index);
+//            index = index + iterationCount;
+            for (int i = 0; i < 10; i++) {
+                Element valueLine = values.get(index + i);
+                for (Element td : valueLine.select("td")) {
+                    System.out.print(td.text() + "\t" + "\t" + "\t");
+                }
+                System.out.println();
+            }
+
+        }
+
+
+    }
+}
